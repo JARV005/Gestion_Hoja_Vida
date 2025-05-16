9b21cd76c70a670d34c8088e209843e23a75f4c1
BrahianMS <johnbra696@gmail.com>
2025-05-16 12:20:15 -0500

    Actualizacion.py

--- actualizacion.py
+++ actualizacion.py
@@ -29,99 +29,107 @@ cvs = {'1020' : {
 
 # (num_id,name,contact,addres,email,birthdate,institute,title,year,company,position,functions,period,ref,relationship,phone,certificates)
 
-def add_list(num_id, site, mod):
-    cvs[num_id][site].append(mod)
-
-def add_set(num_id, site, mod):
-    cvs[num_id][site].add(mod)
-
-
 def act_datos():
-    num_id = str(input("Ingrese el número de num_idumento: "))
-    if num_id not in cvs:
+    id = str(input("Ingrese el número de num_idumento: "))
+    if id not in cvs:
         print("Ese numero de documento no se encuentra registrado")
     else:
         print("\nSeleccione la acción que desea realizar:")
-        print("1. Añadir dato\n2. Modificar dato")
+        print("1. Añadir dato\n2. Modificar dato\n3. Regresar")
         opc = int(input(">> "))
         while True:
-            if opc == 1:
-                print("\nSeleccione el dato que desea añadir: ")
-                op1 = int(input("1. Estudios\n2. Experiencia\n3. Habilidades\n4. Certificaciones\n5. Regresar"))
-                match op1:
-                    case 1:
-                        while True:
-                            ed = input("Ingrese el estudio que desea añadir: ")
-                            for dato in cvs[num_id]["Estudios"]:
-                                if ed in dato:
-                                    print("El estudio ingresado ya existe")
-                                    cont = False
-                                    break
-                            if not cont:
+            match opc:
+                case 1:
+                    while True:
+                        print("\nSeleccione el dato que desea añadir: ")
+                        op1 = int(input("1. Estudios\n2. Experiencia\n3. Habilidades\n4. Certificaciones\n5. Regresar"))
+                        match op1:
+                            case 1:
+                                while True:
+                                    cont = True
+                                    title = input("Ingrese el título que desea añadir: ")
+                                    for dato in cvs[id]["Estudios"]:
+                                        if title in dato:
+                                            print("El estudio ingresado ya existe")
+                                            cont = False
+                                            break
+                                    if not cont:
+                                        break
+                                    else:
+                                        institute = input("Nombre de la institución: ")
+                                        year = input("Periodo de estudios (ej. 2020-2024): ")
+                                        estudios =[institute,title,year]
+                                        cvs[id]["Estudios"].append(estudios)
+                                        break
+                            case 2:
+                                while True:
+                                    cont = True
+                                    position = input("Ingrese el cargo en la empresa: ")
+                                    for dato in cvs[id]["Experiencia"]:
+                                        if position in dato:
+                                            print("El cargo ingresado ya existe")
+                                            cont = False
+                                            break
+                                    if not cont:
+                                        break
+                                    else:
+                                        company = input("nombre de la empresa: ")
+                                        functions = input("funciones que desempeñaba: ")
+                                        period = input("periodo en el que trabajó: ")
+                                        experiencia = [company, position, functions, period]
+                                        cvs[id]["Experiencia"].append(experiencia)
+                                        break
+                            case 3:
+                                while True:
+                                    hab = input("Ingrese la habilidad que desea añadir: ")
+                                    if hab in cvs[id]["Habilidades"]:
+                                        print("La habilidad ingresada ya existe")
+                                        break
+                                    else:
+                                        cvs[id]["Habilidades"].add(hab)
+                                        break
+                            case 4:
+                                while True:
+                                    cont = True
+                                    name_certificate = input("Nombre del certificado: ")
+                                    for dato in cvs[id]["Certificaciones"]:
+                                        if name_certificate in dato:
+                                            print("La certificación ingresada ya existe")
+                                            cont = False
+                                            break
+                                    if not cont:
+                                        break
+                                    else:
+                                        institute_c = input("Institucion")
+                                        name_certificate = input("Nombre del certificado: ")
+                                        year_certificate = input("Añoi de realización: ")
+                                        certificates = [institute_c,name_certificate,year_certificate]
+                                        cvs[id]["Certificaciones"].append(certificates)
+                                        break
+                            case 5:
                                 break
-                            else:
-                                fecha = input("Ingrese el año de inicio finalización en el siguiente formato (AAAA-AAAA): ")
-                                nueva_ed = [ed, fecha]
-                                cvs[num_id]["Estudios"].append(nueva_ed)
+                            case _:
+                                print("Debe elegir una de las opciones")
+                case 2:
+                    while True:
+                        print("\n¿Qué dato desea cambiar?")
+                        print("1. Teléfono\n2. Dirección\n3. Correo Electronico\n4. Regresar")
+                        opd = int(input(">> "))
+                        match opd:
+                            case 1:
+                                contact = input("Ingrese el nuevo número de telefono: ")
+                                cvs[id]["Datos Personales"]["Telefono"]= contact
+                            case 2:
+                                addres = input("Ingrese la nueva dirección: ")
+                                cvs[id]["Datos Personales"]["Dirección"]= addres
+                            case 3:
+                                email = input("Ingrese el nuevo correo electrónico: ")
+                                cvs[id]["Datos Personales"]["Correo Electronico"]= email
+                            case 4:
                                 break
-                    case 2:
-                        while True:
-                            exp = input("Ingrese el cargo que desea añadir: ")
-                            for dato in cvs[num_id]["Experiencia"]:
-                                if exp in dato:
-                                    print("El cargo ingresado ya existe")
-                                    cont = False
-                                    break
-                            if not cont:
-                                break
-                            else:
-                                fecha = input("Ingrese la fecha de inicio y fin en el siguiente formato (AAA-MM): ")
-                                nueva_exp = [exp, fecha]
-                                cvs[num_id]["Experiencia"].append(nueva_exp)
-                                break
-                    case 3:
-                        while True:
-                            hab = input("Ingrese la habilidad que desea añadir: ")
-                            if hab in cvs[num_id]["Habilidades"]:
-                                print("La habilidad ingresada ya existe")
-                                break
-                            cvs[num_id]["Habilidades"].add(hab)
-                            break
-                    case 4:
-                        while True:
-                            cont = True
-                            cert = input("Ingrese la nueva certificación: ")
-                            for dato in cvs[num_id]["Certificaciones"]:
-                                if cert in dato:
-                                    print("La certificación ingresada ya existe")
-                                    cont = False
-                                    break
-                            if not cont:
-                                break
-                            else:
-                                cvs[num_id]["Certificaciones"].append(cert)
-                                break
-                    case 5:
-                        break
-                    case _:
-                        print("Debe elegir una de las opciones")
-            if opc == 2:
-                print("\n¿Qué dato desea cambiar?")
-                print("1. Teléfono\n2. Dirección\n3. Correo")
-                opd = int(input(">> "))
-                match opd:
-                    case 1:
-                        contact = input("Ingrese el nuevo número de telefono: ")
-                        cvs[num_id]["Datos Personales"]["Telefono"]= contact
-                    case 2:
-                        contact = input("Ingrese la nueva dirección: ")
-                        cvs[num_id]["Datos Personales"]["Dirección"]= contact
-                    case 3:
-                        contact = input("Ingrese el nuevo correo electrónico: ")
-                        cvs[num_id]["Datos Personales"]["Correo"]= contact
-                    case _:
-                        print("MAL")
-            
-
-
-act_datos()
+                            case _:
+                                print("Error: Debe seleccinar una de las opciones")
+                case 3:
+                    break
+                case _:
+                    print("Error: Debe seleccinar una de las opciones")
